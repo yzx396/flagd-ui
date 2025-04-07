2780cb6cfa2077cc93490fcd1309a651d55df86c
yzx <jenson.zhao@gmail.com>
2025-04-06 23:08:01 -0700

    Update the base url in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://yangzhaox.github.io/flagd-ui/'
+  base: 'https://yzx396.github.io/flagd-ui/'
 })
